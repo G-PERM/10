8ffcfa5beaa4526e5a27f58ee244b4447bec3a8a
G-PERM <gpermweb@gmail.com>
2024-12-31 00:03:16 +0530

    ResponsiveNav

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function LogoLink() {
-  window.open("index.html" /* , "_blank"*/);
+function sidebar() {
+  let sidebar = document.getElementsByClassName("sidebar")[0];
+  sidebar.style.display = "flex";
+}
+function nosidebar() {
+  let sidebar = document.getElementsByClassName("sidebar")[0];
+  sidebar.style.display = "none";
 }
